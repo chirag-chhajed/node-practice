906a2381ca8d0d0e4a6e9ab92340eee9ec508eae
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-17 18:51:36 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+// 
+const names = require('./names')
+const sayHi = require('./function')
+const data = require("./alternative")
+require("./mind-grinade")
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
+
