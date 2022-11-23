0f1d6a7f543bf2951030b5da1f5c15e8340b2d24
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-11-23 19:30:25 +0530

    change made

--- node-express-course-/express/app.js
+++ node-express-course-/express/app.js
@@ -1,26 +1,19 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// req => middleware => res
-// app.use([logger,authorize])
-// api/home/about/products
-app.use(morgan('tiny'))
-app.get('/',logger,(req,res)=>{
-    res.send('Home')
-})
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('Products')
-})
-app.get('/api/items',(req,res)=>{
-    console.log(req.users)
-    res.send('Items')
-})
 
-app.listen(5000,()=>{
-    console.log('server is listening on port 5000')
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
+app.use('/api/people', people)
+app.use('/login', auth)
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
